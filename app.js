5077c92d1c9b70c292b130096073539a5e1a0d1e
SujonHossain1 <sujon.hossain758@gmail.com>
2020-12-22 14:04:45 +0600

    solve reactive submit button

--- app.js
+++ app.js
@@ -3,11 +3,22 @@ const showRandomNumber = document.querySelector(".pin-generator .form-control");
 const generatorBtn = document.querySelector(".generate-btn");
 
 let randomNumber = null;
+let tryCount = 3;
+
+const tryAction = document.querySelector('.action-left');
+tryAction.innerText = `${tryCount} try left`;
+tryAction.style.display = 'none';
 
 generatorBtn.addEventListener('click', () => {
     let random = Math.random() * 9000 + 1000;
     showRandomNumber.value = parseInt(random);
     randomNumber = parseInt(random);
+
+    if (tryCount <= 0) {
+        submitBtn.classList.remove('btn-disabled');
+        submitBtn.disabled = false;
+        tryAction.style.display = 'none';
+    }
 
 })
 
@@ -26,7 +37,7 @@ calcParentBody.addEventListener('click', (event) => {
         if (targetValue == 'C') {
             showCalcValue.value = ''
         } else if (targetValue == '<') {
-            showCalcValue.value = formValue.substring(0, formValue.length- (++newCount));
+            showCalcValue.value = formValue.substring(0, formValue.length - (++newCount));
         } else {
             showCalcValue.value += parseInt(targetValue);
             submitNumber = parseInt(showCalcValue.value);
@@ -42,19 +53,13 @@ calcParentBody.addEventListener('click', (event) => {
 const submitBtn = document.querySelector(".submit-btn");
 const notifySuccess = document.querySelector('.notify-success ');
 const notifyFailed = document.querySelector('.notify-failed');
-const tryAction = document.querySelector('.action-left');
 
-let tryCount = 3;
-
-
-tryAction.innerText = `${tryCount} try left`;
-tryAction.style.display = 'none';
 
 
 submitBtn.addEventListener("click", () => {
 
     if (randomNumber === submitNumber) {
-        tryCount = 3;  /* Try Action*/ 
+        tryCount = 3;  /* Try Action*/
         tryAction.style.display = 'none';
 
         notifySuccess.style.display = "block"; /* notify is sucess  */
@@ -66,8 +71,8 @@ submitBtn.addEventListener("click", () => {
         showRandomNumber.value = '';
         showCalcValue.value = '';
     } else {
-        --tryCount;    /* Try Action*/ 
-        
+        --tryCount;    /* Try Action*/
+
         if (tryCount <= 0) {
             submitBtn.classList.add('btn-disabled');
             submitBtn.disabled = true;
